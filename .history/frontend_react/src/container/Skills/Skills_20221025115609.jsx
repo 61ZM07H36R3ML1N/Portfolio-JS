1b8d5532cd7ea9e95f5ea4ffaadf1ebf7889e830
BT (Grem) Jamiel <brianthomasjamiel@gmail.com>
2022-10-25 11:59:50 -0700

    import chunk added

--- .history/frontend_react/src/container/Skills/Skills_20221025115609.jsx
+++ .history/frontend_react/src/container/Skills/Skills_20221025115609.jsx
@@ -3,11 +3,12 @@ import { motion } from 'framer-motion';
 import ReactTooltip from 'react-tooltip';
 
 import { AppWrap } from '../../wrapper';
-import { urlFor, client } from '../../client';
+import { urlFor } from '../../client';
+
 import './Skills.scss';
 
 const Skills = () => {
-  const 
+  return <div>Skills</div>;
 };
 
 export default Skills;
